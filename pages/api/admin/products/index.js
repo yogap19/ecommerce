92221ce03947002216608d26217ceaa8146f06de
Yoga Pramana <ogapma19@gmail.com>
2023-01-25 20:49:45 +0700

    add image

--- pages/api/admin/products/index.js
+++ pages/api/admin/products/index.js
@@ -22,7 +22,7 @@ const postHandler = async (req, res) => {
   const newProduct = new Product({
     name: 'sample name',
     slug: 'sample-name-' + Math.random(),
-    image: '/images/shirt1.jpg',
+    image: '/images/default.jpg',
     price: 0,
     category: 'sample category',
     brand: 'sample brand',
