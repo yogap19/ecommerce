67007e6329747f369e1ac9aadbdbc504e5c5baee
Yoga Pramana <ogapma19@gmail.com>
2023-02-13 16:55:10 +0700

    perubahan judul

--- components/Layout.js
+++ components/Layout.js
@@ -49,7 +49,7 @@ export default function Layout({ title, children }) {
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/" className="text-lg font-bold text-emerald-600">
-              Fashion
+              Toko 000.loved
             </Link>
             <div>
               <form
